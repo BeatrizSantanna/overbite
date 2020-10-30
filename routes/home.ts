c350ea3a3cb9cffa3fe75cd874a99d56d5251642
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2020-10-30 15:33:17 -0300

    Criando a estrutura inicial

---
+++ routes/home.ts
@@ -0,0 +1,14 @@
+﻿import express = require("express");
+import wrap = require("express-async-error-wrapper");
+
+const router = express.Router();
+
+router.all("/", wrap(async (req: express.Request, res: express.Response) => {
+	res.render("home/index");
+}));
+
+router.all("/index2", wrap(async (req: express.Request, res: express.Response) => {
+	res.render("home/index2");
+}));
+
+export = router;
